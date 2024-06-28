5837f914836773e5ece9d786f166eb89d5bffc9c
4792411 <evgeniipusser88@gmail.com>
2024-06-28 22:04:23 +0300

    RedBlack Left Tree

---
+++ src/Node.java
@@ -0,0 +1,24 @@
+public class Node {
+    int value;
+    Node parent, left, right;
+    Color color;
+
+    public int getValue() {
+        return value;
+    }
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public Node getLeft() {
+        return left;
+    }
+
+    public Node getRight() {
+        return right;
+    }
+
+
+}
+
